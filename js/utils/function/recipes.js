97e4141759914df94876d903416d93d18245f4e9
Camille Crapat <camille.capt@gmail.com>
2023-09-23 10:32:26 +0200

    Changement classification des fichiers

--- js/utils/function/recipes.js
+++ js/utils/function/recipes.js
@@ -1,12 +1,12 @@
 function recipes() {
 	// Variables
 	// DOM
-	const recipesEmplacement = document.querySelector('section.recettes');
+	const recipesEmplacement = document.querySelector('section.recettes'); // Emplacement où seront placé les différentes recettes
 
 	// Fonction
 	// Affichage des recettes
 	function displayRecipes() {
-		recipesData.forEach((recipe) => {
+		recipesCurrent.forEach((recipe) => {
 			const recipesModel = recipesTemplate(recipe);
 			const recipesDOM = recipesModel.recipesDOM();
 			recipesEmplacement.appendChild(recipesDOM);
