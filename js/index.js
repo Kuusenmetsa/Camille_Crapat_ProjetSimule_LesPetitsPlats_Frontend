831fa8bff950013b59020b82b96a53657a8489d8
Camille Crapat <camille.capt@gmail.com>
2023-10-28 16:16:37 +0200

    ajout du comptage de recette

--- js/index.js
+++ js/index.js
@@ -5,5 +5,8 @@ function init() {
 	filters();
 	search();
 	recipes().displayRecipes(); // Chargement des recettes
+	const count = document.querySelectorAll('.recettes > article').length;
+	const el = document.querySelector('.numberOfRecipes');
+	el.textContent = `${count} recette${count > 1 ? 's' : ''}`;
 }
 init();
